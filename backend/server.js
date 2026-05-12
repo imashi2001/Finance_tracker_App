5b208934e31b53a5ef658f1aa9d853c5a8cfc38a
imashi2001 <ranaweeraimashi880@gmail.com>
2026-05-13 00:39:43 +0530

    Add server.js and update .gitignore to include environment variables

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Finance Tracker API Running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("MongoDB Connected");
+
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+})
+.catch((err) => {
+    console.log(err);
+});
